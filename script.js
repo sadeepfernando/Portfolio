a963b0c8603c254caaa4f06250f6a85ddaa4e2cf
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-07-30 16:38:15 +0530

    Refactor menu toggle functionality and update styles

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const menuIcon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    menuIcon.classList.toggle("open");
+    }
